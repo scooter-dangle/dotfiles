354515d087bed1b3953f2c21afd4a3c6309e9178
scooter-dangle <scottlsteele@gmail.com>
2013-05-09 19:52:10 -0400

    Add .fish files

---
+++ aliases.fish
@@ -0,0 +1,20 @@
+# Tip from M Subelsky:
+function ea; vim ~/.config/fish/aliases.fish; and . ~/.config/fish/aliases.fish; end
+function ag; grep $argv[1] ~/.config/fish/aliases.fish; end
+
+function up; cd ..; end
+function bk; cd - ; end
+function hm; cd ~ ; end
+
+function bx; bundle exec; end
+function rsh; pry --require rake; end
+
+function gs; git status -s; end
+function gd; git diff -b --color; end
+function gl; git log; end
+function gb; git branch; end
+function gp; git pull; end
+function GP; git push; end
+
+function s;  grep $argv[1] -Rn . | less; end
+function md;  mkdir $argv[1]; and cd $argv[1]; end
---
+++ config.fish
@@ -0,0 +1,3 @@
+if status --is-interactive
+        . ~/.config/fish/aliases.fish
+end
