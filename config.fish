6f9967715942160af7cf87981a7c20b832205bb3
scooter-dangle <scottlsteele@gmail.com>
2013-05-11 17:42:02 -0400

    Change fish `set` option to -x
    Super important to do this otherwise ruby won't see $GEM_HOME in ENV and
    none of your gems will load.

--- config.fish
+++ config.fish
@@ -4,13 +4,14 @@ if status --is-interactive
     . ~/.config/fish/aliases.fish
 end
 
-set NODE_PATH /usr/local/lib/node /usr/local/lib/node_modules /usr/lib/node /usr/lib/node_modules
+set -x NODE_PATH /usr/local/lib/node /usr/local/lib/node_modules /usr/lib/node /usr/lib/node_modules
 
-set PATH ~/erlang/rebar $PATH
-set PATH ~/.cabal/bin $PATH
+set -x PATH ~/erlang/rebar $PATH
+set -x PATH ~/.cabal/bin $PATH
+set -x PATH $HOME/.rbenv/bin $PATH
 
-set GEM_HOME $HOME/.gems
-set PATH .bundle/bin $GEM_HOME/bin $PATH
+set -x GEM_HOME $HOME/.gems
+set -x PATH .bundle/bin $GEM_HOME/bin $PATH
 
 function bundle-bootstrap
     bundle install --binstubs=.bundle/bin path=.bundle/gems
