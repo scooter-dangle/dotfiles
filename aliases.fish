ca252390fffd4b800bfcdbcf733c7483c9a0032b
scooter-dangle <scottlsteele@gmail.com>
2013-07-14 20:38:15 -0400

    More tweaks

--- aliases.fish
+++ aliases.fish
@@ -3,13 +3,18 @@
 # Tip from M Subelsky:
 set -u aliases ~/.config/fish/aliases.fish
 function ea; vim $aliases; and . $aliases; end
-function ag; if test (grep "function "$argv[1] $aliases); set target "function "$argv[1]; else; set target $argv[1]; end; for line in (grep $target $aliases 2>/dev/null | pp_fish_aliases); echo $line | grep $target; or echo $line; end; end
+function ag; if test (grep "function "$argv[1] $aliases); set target "function "$argv[1]; else; set target $argv[1]; end; for line in (grep $target $aliases 2>/dev/null | pp_fish_aliases); echo $line | grep $target; or echo $line; end | pygmenter; end
+function al; pp_fish_aliases $aliases | pygmenter | more; end
 
 # helper
 function pp_fish_aliases; sed -ue "s/\([^)]\);\s*/\1\n/g" $argv[1] | fish_indent; end
-
 # helper
-function with_silent_errors; echo '2>/dev/null'; end
+function prettify_aliases -d "Prettifies contents of aliases.fish"; pp_fish_aliases $aliases > $aliases; end
+# helper
+# Still under construction
+function uglify_aliases -d "Uglifies contents of aliases.fish"; end
+# helper
+function pygmenter -d "Runs argument through pygmentize -lbash only if pygmentize can be found"; if test (which pygmentize); pygmentize -lbash $argv[1]; else; cat $argv[1]; end; end
 
 function up; cd ..; end
 function bk; cd - ; end
@@ -25,6 +30,7 @@ function gb; git branch $argv; end
 function gp; git pull $argv; end
 function GP; git push $argv; end
 
+function pc; pygmentize $argv[1]; end
 
 function s -d "Find the given argument in any file within the current directory or its subdirectories";  grep $argv[1] -Rin . | more; or echo $argv[1] not found; end
 function md;  mkdir $argv[1]; and cd $argv[1]; end
