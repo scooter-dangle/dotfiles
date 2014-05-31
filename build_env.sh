8ee51ec46f9b134b1ff231f4b34d4832b30252f1
Scott Steele <scottlsteele@gmail.com>
2014-05-31 12:53:57 -0400

    Re-arrange build_env.sh

--- build_env.sh
+++ build_env.sh
@@ -2,18 +2,22 @@
 # build_env.sh
 
 sudo apt-get -y install vim-common tmux fish git htop exuberant-ctags build-essential rbenv
+
 cd ~
 git clone https://github.com/scooter-dangle/dotfiles.git
-git clone https://github.com/scooter-dangle/dotvim.git
-mv dotvim .vim
+ln --symbolic ~/dotfiles/.tmux.conf   ~/.tmux.conf
+
 mkdir --parents .config/fish
 ln --symbolic ~/dotfiles/config.fish  ~/.config/fish/config.fish
 ln --symbolic ~/dotfiles/aliases.fish ~/.config/fish/aliases.fish
-ln --symbolic ~/dotfiles/.tmux.conf   ~/.tmux.conf
-ln --symbolic ~/.vim/vimrc ~/.vimrc
 fish --command=install_rbenv
 fish --command=fish_update_completions
+
+git clone https://github.com/scooter-dangle/dotvim.git
+mv dotvim .vim
+ln --symbolic ~/.vim/vimrc ~/.vimrc
 cd ~/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim.git
 vim +PluginInstall +qall
+
 cd ~
