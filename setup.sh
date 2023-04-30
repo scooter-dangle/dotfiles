3d2429ba19defb245a8c12821094fa7c173de4d2
Scott Steele <scottlsteele@gmail.com>
2023-04-29 22:46:25 -0400

    initial setup.sh

---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# apt tool installs
+apt update --yes
+apt install --yes \
+  curl \
+  fish \
+  git \
+  ripgrep \
+  fd-find
+  bat \
+  tmux \
+  jq \
+  icdiff \
+  neovim \
+  build-essential
+
+# Rust
+bash <( \
+  curl --proto '=https' \
+    --tlsv1.2 \
+    --silent \
+    --show-error \
+    --fail \
+    https://sh.rustup.rs \
+) -y
+
+# Rust tools
+cargo install \
+  cargo-expand
+
+# symlinks
+
+mkdir --parents ~/.config/fish
+ln --symbolic ~/dotfiles/fish/config.fish  ~/.config/fish/config.fish
+ln --symbolic ~/dotfiles/fish/aliases.fish ~/.config/fish/aliases.fish
+
+for file in .bash* .gitconfig .tmux.conf .gitignore_global .tmux.conf
+do
+  ln --symbolic ~/dotfiles/$file  ~/$file
+done
+
+# use fish!
+chsh --shell $(which fish)
